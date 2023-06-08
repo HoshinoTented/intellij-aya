dfcd352d95b30ec4ee397e18089d08fb59cae21b
HoshinoTented <hoshinotented@qq.com>
2023-06-08 22:14:29 +0800

    misc: add logger and fix no project tree view after open project

--- src/main/java/org/aya/intellij/actions/lsp/AyaStartup.kt
+++ src/main/java/org/aya/intellij/actions/lsp/AyaStartup.kt
@@ -1,21 +1,16 @@
 package org.aya.intellij.actions.lsp
 
-import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.BaseProjectDirectories.Companion.getBaseDirectories
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.vfs.VirtualFile
 import org.aya.generic.Constants
 import org.aya.intellij.service.AyaSettingService
-import org.aya.lsp.utils.Log
 
 class AyaStartup : ProjectActivity {
   override suspend fun execute(project: Project) {
-    if (AyaSettingService.getInstance().ayaLspState == AyaSettingService.AyaState.UseIntegration) {
-      Log.i("Using aya integration, skipping AyaStartup.")
-      return
-    }
-    if (AyaSettingService.getInstance().ayaLspState != AyaSettingService.AyaState.Enable) return
+    if (AyaSettingService.getInstance().ayaLspState == AyaSettingService.AyaState.Disable) return
+    if (AyaLsp.isActive(project)) return
     val ayaJson = findAyaJson(project)
     if (ayaJson != null) {
       if (!JB.fileSupported(ayaJson)) return
@@ -24,12 +19,9 @@ class AyaStartup : ProjectActivity {
   }
 
   private fun findAyaJson(project: Project): VirtualFile? {
-    val mods = ModuleManager.getInstance(project).modules
-    if (mods.size != 1) return null
-    val mod = mods[0]
-    val contentRoots = ModuleRootManager.getInstance(mod).contentRoots
-    if (contentRoots.size != 1) return null
-    val root = contentRoots[0]
-    return root.findChild(Constants.AYA_JSON)
+    return project.getBaseDirectories()
+      .takeIf { it.size == 1 }
+      ?.firstOrNull()
+      ?.findChild(Constants.AYA_JSON)
   }
 }
